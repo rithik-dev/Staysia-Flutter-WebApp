5bd2ae7e5b42b393c976da5553302c5f56d3fde5
Rithik Bhandari <rithikbhandari895@gmail.com>
2020-11-18 20:23:40 +0530

    added dio configuration and log interceptor

--- lib/utils/get_dio.dart
+++ lib/utils/get_dio.dart
@@ -1,5 +1,15 @@
 import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 Dio getDioInstance() {
-  return Dio(BaseOptions(baseUrl: 'https://staysia.herokuapp.com/'));
+  return Dio(
+    BaseOptions(
+      baseUrl: 'https://staysia.herokuapp.com/',
+      headers: {
+        'Authorization': 'Bearer Token Here',
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+      contentType: Headers.formUrlEncodedContentType,
+    ),
+  )..interceptors.add(PrettyDioLogger(requestBody: true));
 }
