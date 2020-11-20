eff76cee6250a20bafce9d32e9d016ee51315ada
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-11-21 01:52:15 +0530

    - added home components

---
+++ lib/components/google_sign_in_button.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:staysia_web/controller/user_controller.dart';
+import 'package:staysia_web/utils/constants.dart';
+import 'package:staysia_web/utils/firebase_auth.dart';
+
+class GoogleButton extends StatefulWidget {
+  @override
+  _GoogleButtonState createState() => _GoogleButtonState();
+}
+
+class _GoogleButtonState extends State<GoogleButton> {
+  bool _isProcessing = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+          side: BorderSide(color: Colors.blueGrey, width: 3),
+        ),
+        color: Colors.white,
+      ),
+      child: OutlineButton(
+        highlightColor: Colors.blueGrey[100],
+        splashColor: Colors.blueGrey[200],
+        onPressed: () async {
+          setState(() {
+            _isProcessing = true;
+          });
+          // ignore: omit_local_variable_types
+          String jwt = await FirebaseAuthService.signInWithGoogle();
+          ACCESS_TOKEN = jwt;
+          // ignore: omit_local_variable_types
+          String result =
+              await UserController.googleSignupController(idToken: jwt);
+          setState(() {
+            _isProcessing = false;
+          });
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+          side: BorderSide(color: Theme.of(context).accentColor, width: 3),
+        ),
+        highlightElevation: 0,
+        // borderSide: BorderSide(color: Colors.blueGrey, width: 3),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: _isProcessing
+              ? CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    Theme.of(context).accentColor,
+                  ),
+                )
+              : Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage('images/google_logo.png'),
+                      height: 30.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Text(
+                        'Continue with Google',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,13 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:staysia_web/components/TopBarContents.dart';
 import 'package:staysia_web/components/bottom_bar.dart';
-import 'package:staysia_web/components/city_card.dart';
+import 'package:staysia_web/components/city_carousel.dart';
 import 'package:staysia_web/components/custom_error_widget.dart';
+import 'package:staysia_web/components/city_heading.dart';
 import 'package:staysia_web/components/explore_drawer.dart';
 import 'package:staysia_web/components/responsive_widget.dart';
+import 'package:staysia_web/components/search_bar.dart';
 import 'package:staysia_web/components/web_scrollbar.dart';
 import 'package:staysia_web/controller/navigation_controller.dart';
-import 'package:staysia_web/models/get_citites.dart';
+import 'package:staysia_web/models/get_cities.dart';
 
 class HomePage extends StatefulWidget {
   static const id = '/home';
@@ -41,7 +44,7 @@ class _HomePageState extends State<HomePage> {
     var screenSize = MediaQuery.of(context).size;
     _opacity = _scrollPosition < screenSize.height * 0.40
         ? _scrollPosition / (screenSize.height * 0.40)
-        : 0;
+        : 1;
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       extendBodyBehindAppBar: true,
@@ -56,9 +59,9 @@ class _HomePageState extends State<HomePage> {
                   Theme.of(context).primaryColor.withOpacity(_opacity),
               elevation: 0,
               title: Text(
-                'EXPLORE',
+                'STAYSIA',
                 style: TextStyle(
-                  color: Colors.blueGrey[100],
+                  color: Theme.of(context).accentColor,
                   fontSize: 20,
                   fontFamily: 'Montserrat',
                   fontWeight: FontWeight.w400,
@@ -81,7 +84,7 @@ class _HomePageState extends State<HomePage> {
               children: [
                 Container(
                   child: SizedBox(
-                    height: screenSize.height * 0.45,
+                    height: screenSize.height * 0.85,
                     width: screenSize.width,
                     child: Image.asset(
                       'images/cover.jpg',
@@ -89,47 +92,31 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
-                Column(
-                  children: [
-                    // FloatingQuickAccessBar(screenSize: screenSize),
-                    Container(
-                      child: Column(
-                        children: const [
-                          // Text('Lol')
-                          // FeaturedHeading(
-                          //   screenSize: screenSize,
-                          // ),
-                          // FeaturedTiles(screenSize: screenSize)
-                        ],
-                      ),
-                    ),
-                  ],
+                Container(
+                  margin: ResponsiveWidget.isLargeScreen(context)
+                      ? EdgeInsets.symmetric(horizontal: screenSize.width * 0.3)
+                      : EdgeInsets.all(20),
+                  child: SearchBar(),
                 )
               ],
             ),
+            DestinationHeading(screenSize: screenSize),
             FutureBuilder<GetCities>(
               future: getCities,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return Container(
-                    height: 300,
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        return CityCard(city: snapshot.data.cities[index]);
-                      },
-                      itemCount: snapshot.data.cities.length,
-                    ),
+                  return CityCarousel(
+                    cities: snapshot.data.cities,
                   );
                 } else if (snapshot.hasError) {
                   return CustomErrorWidget();
                 } else {
-                  return Padding(
-                      padding: EdgeInsets.all(30),
-                      child: CircularProgressIndicator());
+                  return Center(child: CircularProgressIndicator());
                 }
               },
+            ),
+            SizedBox(
+              height: 30,
             ),
             BottomBar()
           ],
