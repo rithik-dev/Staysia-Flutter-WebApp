a5b795f06eaec457f9c939939e9493fc15c3d6d2
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-11-21 16:56:49 +0530

    - updated utils

--- lib/utils/get_dio.dart
+++ lib/utils/get_dio.dart
@@ -3,7 +3,32 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:staysia_web/utils/constants.dart';
 
-Dio getDioInstance() {
+Dio getDioInstance({bool removeBaseHeaders = false}) {
+  if (removeBaseHeaders) {
+    return Dio(
+      BaseOptions(
+        baseUrl: 'https://staysia.herokuapp.com/api/',
+        headers: {
+          'Authorization': 'Bearer $ACCESS_TOKEN',
+        },
+      ),
+    )..interceptors.addAll([
+        PrettyDioLogger(requestBody: true),
+        InterceptorsWrapper(
+          onError: (DioError error) async {
+            if (error.response == null) {
+              // ignore: avoid_print
+              print(error);
+            } else if (error.response.statusCode == 401) {
+              ACCESS_TOKEN = null;
+              final preferences = await SharedPreferences.getInstance();
+              await preferences.remove('jwt');
+              //TODO: push to login page
+            }
+          },
+        ),
+      ]);
+  }
   return Dio(
     BaseOptions(
       baseUrl: 'https://staysia.herokuapp.com/api/',
@@ -17,11 +42,10 @@ Dio getDioInstance() {
       PrettyDioLogger(requestBody: true),
       InterceptorsWrapper(
         onError: (DioError error) async {
-          if(error.response == null){
+          if (error.response == null) {
             // ignore: avoid_print
             print(error);
-          }
-          else if (error.response.statusCode == 401) {
+          } else if (error.response.statusCode == 401) {
             ACCESS_TOKEN = null;
             final preferences = await SharedPreferences.getInstance();
             await preferences.remove('jwt');
