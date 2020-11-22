54ee3fcaa6a496abfee12736bf61b1c08a7cfeeb
Rithik Bhandari <rithikbhandari895@gmail.com>
2020-11-22 20:07:08 +0530

    fixed hotel card

--- lib/components/hotel_card.dart
+++ lib/components/hotel_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:staysia_web/models/hotel.dart';
+import 'package:staysia_web/views/hotel_details_page.dart';
 
 class HotelCard extends StatelessWidget {
   final Hotel hotel;
@@ -8,86 +9,95 @@ class HotelCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)],
-      ),
-      child: Row(
-        children: [
-          Image.network(
-            hotel.thumbnail,
-            height: double.infinity,
-            fit: BoxFit.cover,
-            width: 300,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          hotel.title,
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w600,
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          HotelDetailsPage.id,
+          arguments: hotel.id,
+        );
+      },
+      child: Container(
+        height: 150,
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)],
+        ),
+        child: Row(
+          children: [
+            Image.network(
+              hotel.thumbnail,
+              height: double.infinity,
+              fit: BoxFit.cover,
+              width: 300,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            hotel.title,
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              hotel.neighbourhood,
-                              style: TextStyle(fontSize: 15),
-                            ),
-                            Text(
-                              hotel.city,
-                              style: TextStyle(fontSize: 15),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'Rs ${hotel.price.currentPrice}',
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w600,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                hotel.neighbourhood,
+                                style: TextStyle(fontSize: 15),
                               ),
-                            ),
-                            Row(
-                              children: [
-                                Icon(Icons.star, color: Colors.yellow[700]),
-                                Text(
-                                  hotel.rating.toString(),
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w600,
-                                  ),
+                              Text(
+                                hotel.city,
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Rs ${hotel.price.currentPrice}',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
                                 ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
+                              ),
+                              Row(
+                                children: [
+                                  Icon(Icons.star, color: Colors.yellow[700]),
+                                  Text(
+                                    hotel.rating.toString(),
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
