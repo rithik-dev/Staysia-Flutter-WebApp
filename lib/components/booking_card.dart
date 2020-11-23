fb65aba66fa07389b4a0e310a5f8ba9c00b20054
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-11-23 13:38:35 +0530

    - updated booking card

--- lib/components/booking_card.dart
+++ lib/components/booking_card.dart
@@ -8,6 +8,8 @@ class BookingCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text(booking.title),
+    );
   }
 }
